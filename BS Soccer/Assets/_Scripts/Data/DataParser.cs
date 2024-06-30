dccc8fc3794d1d8cf9290e80fd6f09f7bc49ed0a
nielsduif <31954046+nielsduif@users.noreply.github.com>
2024-06-30 20:27:31 +0200

    first data collector

--- BS Soccer/Assets/_Scripts/Data/DataParser.cs
+++ BS Soccer/Assets/_Scripts/Data/DataParser.cs
@@ -1,11 +1,10 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 /// <summary>
 /// Script that filters out all variables from the raw data file, creating classes with the specific variables stored inside of them
 /// Used https://jsoneditoronline.org/ to easily show all the stored content
-/// AnimationContext and Posse
 /// </summary>
 public class DataParser : MonoBehaviour
 {
@@ -52,9 +51,47 @@ public class DataParser : MonoBehaviour
         public int InjuryTime;
     }
 
-    public class MathScoreContext
+    public class MatchScoreContext
     {
         public int HomeSore;
         public int AwayScore;
+    }
+
+    public class DataFrame
+    {
+        public int FrameCount;
+        public float TimestampUTC;
+        public List<Player> players;
+        public Ball ball;
+        public GameClockContext gameClockContext;
+        public MatchScoreContext matchScoreContext;
+    }
+
+    public static DataParser Instance { get; private set; }
+    [SerializeField] private string fileName = "Applicant-test.idf";
+    public List<DataFrame> frames = new List<DataFrame>();
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        string path = $"{Application.dataPath}/Resources/{fileName}";
+
+        StreamReader sr = new StreamReader(path);
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            print(line);
+        }
     }
 }
