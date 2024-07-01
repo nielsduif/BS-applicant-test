2415618806a3ce97739754d8d17d4a79cff82dd4
nielsduif <31954046+nielsduif@users.noreply.github.com>
2024-07-01 17:01:54 +0200

    Update PlayerManager.cs

--- BS Soccer/Assets/_Scripts/Player/PlayerManager.cs
+++ BS Soccer/Assets/_Scripts/Player/PlayerManager.cs
@@ -7,12 +7,13 @@ public class PlayerManager : MonoBehaviour
     [SerializeField]
     private Player keeper, fieldplayer;
     public static List<Player> players { get; private set; } = new List<Player>();
+    [SerializeField] Transform playerParent;
 
     private void Start()
     {
         foreach (DataParser.Person player in DataParser.Instance.frames[0].Persons)
         {
-            Instantiate(fieldplayer.prefab, new Vector3(player.Position[0], player.Position[1], player.Position[2]), Quaternion.identity, transform);
+            Instantiate(fieldplayer.prefab, new Vector3(player.Position[0], player.Position[1], player.Position[2]), Quaternion.identity, playerParent);
         }
     }
 }
